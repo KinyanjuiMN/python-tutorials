87d3745921583ac4fdc2462f8d5953c2a53a64c3
KinyanjuiMN <knjonjo@gmail.com>
2017-03-26 18:04:46 +0300

    The Bank package  has issues
    @kenju254 the Accounts and People has issues - The import Error
    persists.

---
+++ Animals/mammals.py
@@ -0,0 +1,5 @@
+class mammals(object):
+      def _init_(self,name):
+          self._name = name
+      def _show_mammal(self):
+          print "%s \n" % self._name
---
+++ Bank/People/test.py
@@ -0,0 +1,5 @@
+import People.Cperson.person
+
+per = person()
+per._int("John","23424")
+per._show_person()
